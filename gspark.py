da688323d9343041a8286453642e0766fc81ccad
jxl4650152 <460275039@qq.com>
2018-04-14 18:00:07 +0800

    4.14 v1

--- gspark.py
+++ gspark.py
@@ -5,6 +5,9 @@ from flask_socketio import SocketIO
 import json
 import random
 import time
+import threading
+
+
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -12,6 +15,7 @@ socketio = SocketIO(app)
 historyJobs = []
 runningJobs = []
 CloudWidth = 280
+rand = []
 
 
 @app.route('/')
@@ -56,37 +60,121 @@ def historyjob(jobID):
 @socketio.on('connect')
 def connect_handler():
 
-    print "Client  from  ", request.remote_addr, " connected with sid ", request.sid
+    # def random_event(io, sid):
+    #     lock = threading.Lock()
+    #     cond = threading.Condition(lock=lock)
+    #
+    #     with app.test_request_context():
+    #         while True:
+    #             cond.acquire()
+    #             cond.wait(timeout=4)
+    #             print "thread running", sid
+    #             event1 = json.load(open(unicode("/home/LAB/jiaxl/gsparkdata-exch\\task_launch_to_siteDriver.json", "utf8")))
+    #             event2 = json.load(open(unicode("/home/LAB/jiaxl/gsparkdata-exch\outter_shuffle.json", "utf8")))
+    #             event3 = json.load(open(unicode("/home/LAB/jiaxl/gsparkdata-exch\\task_launch_to_executor.json", "utf8")))
+    #             event4 = json.load(open(unicode("/home/LAB/jiaxl/gsparkdata-exch\inner_shuffle.json", "utf8")))
+    #
+    #             res1 = eventConvert(event1)
+    #             res2 = eventConvert(event2)
+    #             res3 = eventConvert(event3)
+    #             res4 = eventConvert(event4)
+    #
+    #             event_list = [res1, res2, res3, res4]
+    #
+    #             num = random.randint(0, 3)
+    #             res = event_list[num]
+    #             print res
+    #             io.emit(res["event"], res["data"], room=sid)
+    #
+    #             cond.wait(timeout=4)
+    #             cond.release()
 
+
+    print "Client  from ", request.remote_addr, " connected with sid ", request.sid
+
+
+@socketio.on('recieved')
+def connect_handler(data):
+    print "recieved data:", data, type(data)
+    global rand
+    stage0 = "/home/LAB/jiaxl/gspark/data-exch/task_launch_to_siteDriver.json"
+    stage1 = "/home/LAB/jiaxl/gspark/data-exch/task_launch_to_executor"
+    stage2 = "/home/LAB/jiaxl/gspark/data-exch/inner_shuffle"
+    stage3 = "/home/LAB/jiaxl/gspark/data-exch/outter_shuffle"
+    event = {}
+
+    if data["stage"] == 0:
+        event = json.load(open(stage0))
+
+    elif data["stage"] == 1:
+        time.sleep(1)
+        stage1 = stage1 + str(data["count"]) + ".json"
+        event = json.load(open(stage1))
+
+    elif data["stage"] == 2:
+        time.sleep(1)
+        while len(rand) < 7:
+            i = random.randint(0, 5)
+            if i not in rand:
+                rand.append(i)
+                stage2 = stage2 + str(i) + ".json"
+                event = json.load(open(stage2))
+                break
+
+
+    elif data["stage"] == 3:
+        time.sleep(2)
+        stage3 = stage3 + str(data["count"]) + ".json"
+        event = json.load(open(stage3))
+
+    res = eventConvert(event)
+
+    socketio.emit(res["event"], res["data"], room=request.sid)
+    print "recieved reply to", request.sid
+    print " "
+
+@socketio.on('disconnect')
+def connect_handler():
+
+    print "Client  from ", request.remote_addr, " disconnected with sid ", request.sid
+
+
+@socketio.on('close')
+def connect_handler():
+    global rand
+    rand = []
 
 
 @socketio.on('ToSiteDriver')
 def test_handler():
-    event = json.load(open(unicode("D:\课题组相关\data-exch\\task_launch_to_siteDriver.json", "utf8")))
+    #print request.sid
+    event = json.load(open("/home/LAB/jiaxl/gspark/data-exch/task_launch_to_siteDriver.json"))
     res = eventConvert(event)
     socketio.emit(res["event"], res["data"], room=request.sid)
 
 @socketio.on('OuterShuffle')
 def test_handler():
-    event = json.load(open(unicode("D:\课题组相关\data-exch\outter_shuffle.json", "utf8")))
+    #print request.sid
+    event = json.load(open("/home/LAB/jiaxl/gspark/data-exch/outter_shuffle.json"))
     res = eventConvert(event)
     socketio.emit(res["event"], res["data"], room=request.sid)
 
 @socketio.on('ToExecutor')
 def test_handler():
-    event = json.load(open(unicode("D:\课题组相关\data-exch\\task_launch_to_executor.json", "utf8")))
+    #print request.sid
+    event = json.load(open("/home/LAB/jiaxl/gspark/data-exch/task_launch_to_executor.json"))
     res = eventConvert(event)
     socketio.emit(res["event"], res["data"], room=request.sid)
 
 @socketio.on('InnerShuffle')
 def test_handler():
-    event = json.load(open(unicode("D:\课题组相关\data-exch\inner_shuffle.json", "utf8")))
+    #print request.sid
+    event = json.load(open("/home/LAB/jiaxl/gspark/data-exch/inner_shuffle.json"))
     res = eventConvert(event)
     socketio.emit(res["event"], res["data"], room=request.sid)
 
-
 def getJob(id):
-    clu_list = json.load(open(unicode("D:\课题组相关\data-exch\clusters_defination.json", "utf8")))
+    clu_list = json.load(open("/home/LAB/jiaxl/gspark/data-exch/clusters_defination.json"))
     job = Job(clouds=clu_list)
     return job
 
@@ -101,7 +189,7 @@ def eventConvert(event):
             for component in host["from-components"]:
                 res["data"]["executors"].append([host["from-host"] + "-" + component["from-component"], len(component["blocks"])])
 
-        print "EE", res
+
         return res
 
     if event["Event"] == "SparkListenerOutterShuffle":
@@ -112,7 +200,7 @@ def eventConvert(event):
             for component in host["from-components"]:
                 res["data"]["site_array"].append([component["from-component"], len(component["blocks"])])
 
-        print "SS", res
+
         return res
 
     if event["Event"] == "SparkListenerTaskLaunchToExecutor":
@@ -123,7 +211,7 @@ def eventConvert(event):
             for component in host["to-components"]:
                 res["data"]["executors"].append([host["to-host"] + "-" + component["to-component"], len(component["tasks"])])
 
-        print "SE", res
+
         return res
 
     if event["Event"] == "SparkListenerTaskLaunchToSiteDriver":
@@ -134,8 +222,9 @@ def eventConvert(event):
             for component in host["to-components"]:
                 res["data"].append([component["to-component"], len(component["tasks"])])
 
-        print "GS", res
+
         return res
+
 
 
 
@@ -172,5 +261,10 @@ test_data1 = [
         ]
 
 
+
+
+
 if __name__ == '__main__':
+
     socketio.run(app)
+
